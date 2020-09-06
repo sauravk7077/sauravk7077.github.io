32caf3460ab458f35fa10fb9d8af22286d6fcbe7
Saurav Kumar <sauravk7077@gmail.com>
2020-09-06 07:55:54 +0530

    Added required files

---
+++ precache-manifest.72bb141acb0beda83588ccf957133676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552fec461b0127e8245fa81dc5ff5aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc22bd4e21832141b5ac",
+    "url": "/static/css/main.035b1bd1.chunk.css"
+  },
+  {
+    "revision": "824b3692e645f9215a18",
+    "url": "/static/js/2.bc354a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc354a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc22bd4e21832141b5ac",
+    "url": "/static/js/main.770aad24.chunk.js"
+  },
+  {
+    "revision": "b758d9699cd5f0b1e4e4",
+    "url": "/static/js/runtime-main.d54be8d5.js"
+  }
+]);
